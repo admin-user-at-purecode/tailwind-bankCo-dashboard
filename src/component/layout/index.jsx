da3a535e976f2d89b546f885371c8d957f8ae99e
mohammed067 <mohammedrahaman596@gmail.com>
2024-02-21 16:30:40 +0530

    support page completed

--- src/component/layout/index.jsx
+++ src/component/layout/index.jsx
@@ -5,6 +5,7 @@ import HeaderOne from "./header/HeaderOne";
 import HeaderTwo from "./header/HeaderTwo";
 import { useState } from "react";
 import { createContext } from "react";
+import Support from "../../pages/supportTicket";
 
 export const ThemeContext = createContext("");
 
@@ -37,6 +38,7 @@ function Layout({ bg, overlay }) {
                     >
                         <HeaderOne handleSidebar={() => setSidebar(!sidebar)} />
                         <HeaderTwo handleSidebar={() => setSidebar(!sidebar)} />
+                        <Support />
                     </div>
                 </div>
             </div>
