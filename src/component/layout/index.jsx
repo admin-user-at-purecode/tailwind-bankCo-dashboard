b6e5806c482ec3cb24e20b93782df297ef621da3
mohammed067 <mohammedrahaman596@gmail.com>
2024-02-21 13:32:53 +0530

    my wallet page added

--- src/component/layout/index.jsx
+++ src/component/layout/index.jsx
@@ -5,6 +5,7 @@ import HeaderOne from "./header/HeaderOne";
 import HeaderTwo from "./header/HeaderTwo";
 import { useState } from "react";
 import { createContext } from "react";
+import MyWallet from "../../pages/myWallet";
 
 export const ThemeContext = createContext("");
 
@@ -37,6 +38,7 @@ function Layout({ bg, overlay }) {
                     >
                         <HeaderOne handleSidebar={() => setSidebar(!sidebar)} />
                         <HeaderTwo handleSidebar={() => setSidebar(!sidebar)} />
+                        <MyWallet/>
                     </div>
                 </div>
             </div>
